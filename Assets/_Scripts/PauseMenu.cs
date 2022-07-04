cf8ed272fa041abda36746cb6494ad6bedc4a3f9
rhit-yangh6 <yangh6@rose-hulman.edu>
2022-07-03 23:55:27 -0400

    Fully Working PauseMenu

---
+++ Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Scripts
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        public static bool gameIsPaused = false;
+        public GameObject pauseMenuUI;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (gameIsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        public void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            gameIsPaused = false;
+        }
+
+        public void Pause()
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            gameIsPaused = true;
+        }
+
+        public void LoadMenu()
+        {
+            Resume();
+            SceneManager.LoadScene("MenuScene");
+        }
+
+        public void Restart()
+        {
+            Resume();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
