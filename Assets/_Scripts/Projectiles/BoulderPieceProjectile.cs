9ee08867218fbaf8d287a226308fe8166580c734
rhit-yangh6 <yangh6@rose-hulman.edu>
2022-07-12 02:14:18 -0400

    New Package Imported, As the entirety of boulder is finished

---
+++ Assets/_Scripts/Projectiles/BoulderPieceProjectile.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace _Scripts.Projectiles
+{
+    public class BoulderPieceProjectile : MonoBehaviour, IProjectile
+    {
+        public int Level { get; set; }
+        private static float _radius, _damage;
+
+        private Rigidbody2D _rb;
+
+        [SerializeField] private GameObject explosionFX;
+        
+        private void Start()
+        {
+            _rb = gameObject.GetComponent<Rigidbody2D>();
+        }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.CompareTag("DangerZone"))
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Detonate();
+            }
+        }
+
+        private void Update()
+        {
+            Vector2 velocity = _rb.velocity;
+            float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+        public void Detonate()
+        {
+            Vector2 pos = transform.position;
+            DamageHandler.i.HandleCircularDamage(pos, _radius, _damage);
+
+            TerrainDestroyer.Instance.DestroyTerrain(pos, _radius);
+        
+            SpawnExplosionFX();
+            DoCameraShake();
+            
+            Destroy(gameObject);
+        }
+
+        public void SpawnExplosionFX()
+        {
+            GameObject insExpl = Instantiate(explosionFX, transform.position, quaternion.identity);
+            insExpl.transform.localScale *= _radius;
+            Destroy(insExpl, .15f);
+        }
+
+        public void DoCameraShake()
+        {
+            Camera.main.GetComponent<CameraShake>().shakeDuration = .15f;
+        }
+
+        public void SetParameters(float damage, float radius, float maxMagnitude, int steps, ExtraWeaponTerm[] extraWeaponTerms)
+        {
+            _damage = damage;
+            _radius = radius;
+        }
+
+        public float GetMaxMagnitude()
+        {
+            return 0;
+        }
+
+        public int GetSteps()
+        {
+            return 0;
+        }
+
+        public float GetFixedMagnitude()
+        {
+            return -1f;
+        }
+    }
+}
--- Assets/_Scripts/Projectiles/BoulderProjectile.cs
+++ Assets/_Scripts/Projectiles/BoulderProjectile.cs
@@ -1,15 +1,20 @@
+using System;
 using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _Scripts.Projectiles
 {
     public class BoulderProjectile : MonoBehaviour, IProjectile
     {
         public int Level { get; set; }
-
+        
+        [SerializeField] private GameObject explosionFX;
+        [SerializeField] private GameObject boulderPiecePrefab;
+        
         private static float _radius, _damage, _maxMagnitude;
         private static int _steps;
-        public GameObject explosionFX;
+        private static float _boulderPieceRadius, _boulderPieceDamage;
         
         private void OnCollisionEnter2D(Collision2D col)
         {
@@ -27,23 +32,21 @@ namespace _Scripts.Projectiles
         {
             Vector2 pos = transform.position;
 
-
-            
             // Gaining from Higher Levels
-            float finalCalculatedRadius = _radius;
-            float finalCalculatedDamage = _damage;
-
-            if (Level >= 2)
-            {
-                finalCalculatedRadius *= 1.15f;
-            }
-
+            var finalCalculatedRadius = _radius;
+            var finalCalculatedDamage = _damage;
+            
             if (Level >= 3)
             {
+                finalCalculatedRadius *= 1.15f;
                 finalCalculatedDamage *= 1.20f;
             }
-            
-            
+
+            if (Level == 5)
+            {
+                finalCalculatedRadius *= 1.40f;
+                finalCalculatedDamage *= 1.35f;
+            }
 
             DamageHandler.i.HandleCircularDamage(pos, finalCalculatedRadius, finalCalculatedDamage);
 
@@ -51,7 +54,33 @@ namespace _Scripts.Projectiles
         
             SpawnExplosionFX();
             DoCameraShake();
-        
+
+            // Split into Pieces
+
+            var spawnPieces = false;
+            if (Level >= 4) spawnPieces = Random.value > 0.8;
+
+            if (Level == 6) spawnPieces = true;
+
+            if (spawnPieces)
+            {
+                // First Piece
+                var derivedObject = Instantiate(boulderPiecePrefab, pos, Quaternion.identity);
+                var derivedProjectile = derivedObject.GetComponent<IProjectile>();
+                var derivedRb2d = derivedObject.GetComponent<Rigidbody2D>();
+            
+                derivedProjectile.SetParameters(_boulderPieceDamage, _boulderPieceRadius, 0, 0, null);
+                derivedRb2d.velocity = (Vector2.left + Vector2.up * 2) * 3f;
+            
+                // Second Piece
+                derivedObject = Instantiate(boulderPiecePrefab, pos, Quaternion.identity);
+                derivedProjectile = derivedObject.GetComponent<IProjectile>();
+                derivedRb2d = derivedObject.GetComponent<Rigidbody2D>();
+            
+                derivedProjectile.SetParameters(_boulderPieceDamage, _boulderPieceRadius, 0, 0, null);
+                derivedRb2d.velocity = (Vector2.right + Vector2.up * 2) * 3f;
+            }
+
             Destroy(gameObject);
         }
     
@@ -73,11 +102,22 @@ namespace _Scripts.Projectiles
             _radius = radius;
             _maxMagnitude = maxMagnitude;
             _steps = steps;
+            
+            _boulderPieceDamage = Array.Find(extraWeaponTerms, ewt => ewt.term == "boulderPieceDamage").value;
+            _boulderPieceRadius = Array.Find(extraWeaponTerms, ewt => ewt.term == "boulderPieceRadius").value;
         }
 
         public float GetMaxMagnitude()
         {
-            return _maxMagnitude;
+            var finalCalculatedMagnitude = _maxMagnitude;
+            
+            // TODO: Maybe there's a better practice?
+            if (PlayerData.Instance.GetWeaponLevelFromId(1) >= 2)
+            {
+                finalCalculatedMagnitude *= 1.20f;
+            }
+            
+            return finalCalculatedMagnitude;
         }
 
         public int GetSteps()
@@ -90,4 +130,6 @@ namespace _Scripts.Projectiles
             return -1f;
         }
     }
+
+    
 }
