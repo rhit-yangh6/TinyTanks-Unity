94fdb5bc1ec663a07a179fd5af6b6592b5cee083
rhit-yangh6 <yangh6@rose-hulman.edu>
2024-04-10 23:57:56 -0700

    Double Edged Sword Impl

--- Assets/_Scripts/Projectiles/DoubleEdgedSwordProjectile.cs
+++ Assets/_Scripts/Projectiles/DoubleEdgedSwordProjectile.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using _Scripts.Managers;
-using JetBrains.Annotations;
-using Unity.Mathematics;
 using UnityEngine;
-using Random = UnityEngine.Random;
+using System.Collections;
+using _Scripts.Entities;
+using Unity.VisualScripting;
 
 namespace _Scripts.Projectiles
 {
     public class DoubleEdgedSwordProjectile : LaunchedProjectile
     {
+        [SerializeField] private GameObject swordShadow;
+        
         // Shared Fields
         private static float _radius, _damage, _maxMagnitude, _explosionDuration;
         private static int _steps;
         private static GameObject _explosionFX;
         
         // ExtraFields
-        // private static float _unitDiceDamage;
+        private static float _swordShadowYOffset;
         
         // References
         protected override float Radius => _radius;
@@ -25,6 +27,81 @@ namespace _Scripts.Projectiles
         protected override float ExplosionDuration => _explosionDuration;
         protected override GameObject ExplosionFX => _explosionFX;
         
+        // Other Variables
+        private Rigidbody2D _rb;
+        private Renderer _r;
+        private bool isDetonated;
+    
+        private void Start()
+        {
+            isDetonated = false;
+            _rb = gameObject.GetComponent<Rigidbody2D>();
+            _r = GetComponent<Renderer>();
+        }
+
+        private void Update()
+        {
+            Vector2 velocity = _rb.velocity;
+            float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        
+        public override void Detonate()
+        {
+            if (isDetonated)
+            {
+                return;
+            }
+
+            isDetonated = true;
+            
+            Vector2 pos = transform.position;
+
+            _rb.gravityScale = 0;
+            _rb.velocity = Vector2.zero;
+            _r.enabled = false;
+            
+            StartCoroutine(DoubleEdgedSwordAction(pos));
+            // Destroy(gameObject);
+        }
+        
+        private IEnumerator DoubleEdgedSwordAction(Vector2 pos)
+        {
+            // Spawn FX first
+            SpawnExplosionFX();
+            DoCameraShake();
+            yield return new WaitForSeconds(ExplosionDuration);
+            
+            // Calculate Damage
+            var damagedNumber = DamageHandler.i.HandleDamage(pos, Radius, Damage, DamageHandler.DamageType.Circular);
+            
+            // It doesn't destroy tiles, for now
+            // TerrainDestroyer.instance.DestroyTerrainCircular(pos, Radius);
+
+            if (damagedNumber == 0)
+            {
+                // Spawn another sword at shooter's position
+                var shooterPos = Shooter.transform.position;
+                GameObject insExpl = Instantiate(swordShadow, new Vector2(shooterPos.x, shooterPos.y + _swordShadowYOffset), Quaternion.identity);
+            
+                // Take damage
+                Shooter.GetComponent<BuffableEntity>().TakeDamage(Damage);
+                Destroy(insExpl, ExplosionDuration);
+                Destroy(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+        public override void SpawnExplosionFX()
+        {
+            var pos = transform.position;
+            GameObject insExpl = Instantiate(swordShadow, new Vector2(pos.x, pos.y + _swordShadowYOffset), Quaternion.identity);
+            Destroy(insExpl, ExplosionDuration);
+        }
+
         public override void SetParameters(float damage, float radius, float maxMagnitude, int steps, float explosionDuration,
             ExtraWeaponTerm[] extraWeaponTerms)
         {
@@ -35,6 +112,8 @@ namespace _Scripts.Projectiles
             _explosionDuration = explosionDuration;
             
             _explosionFX = GameAssets.i.gunpowderlessExplosionFX;
+            
+            _swordShadowYOffset = Array.Find(extraWeaponTerms, ewt => ewt.term == "swordShadowYOffset").value;
         } 
     }
     
