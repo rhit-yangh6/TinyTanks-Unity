05a0ba9653cd998d854ab9e5b220100ac5d92252
rhit-yangh6 <yangh6@rose-hulman.edu>
2024-06-08 01:12:48 -0700

    Sacrificial Bond refactor + upgrades

--- Assets/_Scripts/Projectiles/SacrificialBondProjectile.cs
+++ Assets/_Scripts/Projectiles/SacrificialBondProjectile.cs
@@ -2,67 +2,96 @@
 using System.Collections;
 using _Scripts.Entities;
 using _Scripts.Managers;
+using MoreMountains.Feedbacks;
 using UnityEngine;
 
 namespace _Scripts.Projectiles
 {
     public class SacrificialBondProjectile: LaunchedProjectile
     {
+        // Set in Inspector
+        [SerializeField] private float enemyDamage = 20f;
+        [SerializeField] private float healingDamage = 15f;
+        [SerializeField] private float totalDistributedDamage = 55f;
+        [SerializeField] private MMFeedbacks damageAndHealMmFeedbacks;
         
         // Shared Fields
         private static float _radius, _damage, _maxMagnitude, _explosionDuration;
         private static int _steps;
         private static GameObject _explosionFX;
         
-        // ExtraFields
-        private static float _initialGrowDuration, _enemyDamageMultiplier;
-        
         // References
         protected override float Radius => _radius;
-        protected override float Damage => _damage;
+        protected override float Damage
+        {
+            get
+            {
+                return Level switch
+                {
+                    6 => _damage * 1.2f,
+                    >= 2 => _damage * 0.72f,
+                    _ => _damage
+                };
+            }
+        }
+
         protected override float MaxMagnitude => _maxMagnitude;
         protected override int Steps => _steps;
         protected override float ExplosionDuration => _explosionDuration;
         protected override GameObject ExplosionFX => _explosionFX;
-        
-        // Other Variables
-        private Rigidbody2D _rb;
-        private SpriteRenderer _sr;
 
-        private void Start()
+        private float EnemyDamage
         {
-            _sr = GetComponent<SpriteRenderer>();
-            _rb = GetComponent<Rigidbody2D>();
-
-            _rb.velocity = Vector2.zero;
-            _rb.gravityScale = 0;
-            _sr.enabled = false;
-
-            StartCoroutine(StartSacrifice());
+            get
+            {
+                return Level switch
+                {
+                    6 => enemyDamage * 2f,
+                    >= 3 => enemyDamage * 1.4f,
+                    _ => enemyDamage
+                };
+            }
         }
         
-        private IEnumerator StartSacrifice()
+        private void Start()
         {
-            var fxPrefab= Instantiate(ExplosionFX, gameObject.transform.position, Quaternion.identity);
-            
-            yield return new WaitForSeconds(_initialGrowDuration);
-            DoCameraShake();
-            
-            // Deal Damage
+            Rigidbody2D.velocity = Vector2.zero;
+            Rigidbody2D.gravityScale = 0;
+            renderer.enabled = false;
+
+            if (Level >= 4)
+            {                
+                damageAndHealMmFeedbacks.PlayFeedbacks();
+            }
+            else
+            {
+                defaultMmFeedbacks.PlayFeedbacks();
+            }
+        }
+
+        public override void DealDamage()
+        {
             var enemies = GameObject.FindGameObjectsWithTag("Enemy");
             foreach (var enemy in enemies)
             {
-                enemy.GetComponent<BuffableEntity>().TakeDamage(Damage * _enemyDamageMultiplier);
+                if (Level == 5)
+                {
+                    enemy.GetComponent<BuffableEntity>().TakeDamage(totalDistributedDamage / enemies.Length);
+                }
+                else
+                {
+                    enemy.GetComponent<BuffableEntity>().TakeDamage(EnemyDamage);
+                }
             }
             // Sacrifice self, it is always critical hit
             Shooter.GetComponent<BuffableEntity>().TakeDamage(Damage, true);
-            
-            // Wait until explosion ended and destroy the gameObject
-            yield return new WaitForSeconds(ExplosionDuration);
-            Destroy(fxPrefab);
-            Destroy(gameObject);
         }
-        
+
+        public void HealAfterDamage()
+        { 
+            Shooter.GetComponent<BuffableEntity>().TakeDamage(-healingDamage);
+        }
+
         public override void SetParameters(float damage, float radius, 
             float maxMagnitude, int steps, float explosionDuration, ExtraWeaponTerm[] extraWeaponTerms)
         {
@@ -73,9 +102,6 @@ namespace _Scripts.Projectiles
             _explosionDuration = explosionDuration;
 
             _explosionFX = GameAssets.i.sacrificeFX;
-
-            _initialGrowDuration = Array.Find(extraWeaponTerms, ewt => ewt.term == "initialGrowDuration").value;
-            _enemyDamageMultiplier = Array.Find(extraWeaponTerms, ewt => ewt.term == "enemyDamageMultiplier").value;
         }
     }
 }
