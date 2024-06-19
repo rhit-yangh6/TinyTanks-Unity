a317bf807d6f6593993cea45b5edd6114ba6a1fb
rhit-yangh6 <yangh6@rose-hulman.edu>
2024-06-18 21:01:23 -0700

    gear upgrades

--- Assets/_Scripts/Projectiles/GearProjectile.cs
+++ Assets/_Scripts/Projectiles/GearProjectile.cs
@@ -7,28 +7,67 @@ using UnityEngine;
 
 namespace _Scripts.Projectiles
 {
-    public class GearProjectile: LaunchedProjectile
+    public class GearProjectile : LaunchedProjectile
     {
         // Set in Inspector
         [SerializeField] private GameObject gearBackgroundPrefab;
-        [SerializeField] private int maximumGears = 5;
         [SerializeField] private float extraUnitDamageMultiplier = 0.1f;
         [SerializeField] private MMFeedbacks spinMmFeedbacks;
+        [SerializeField] private float gearSpinDamage = 10f;
+        [SerializeField] private float gearExplosionDamage = 25f;
+        [SerializeField] private MMFeedbacks activateMmFeedbacks;
         
-        private static readonly int SpinIndex = Animator.StringToHash("Spin");
-
         // References
-        protected override float Radius => radius;
-        protected override float Damage => damage;
-        protected override float MaxMagnitude => maxMagnitude;
-        protected override int Steps => steps;
+        protected override float Damage
+        {
+            get
+            {
+                return Level switch
+                {
+                    5 => damage * 1.3f,
+                    _ => damage
+                };
+            }
+        }
+
+        protected override float MaxMagnitude
+        {
+            get
+            {
+                return Level switch
+                {
+                    >= 2 => maxMagnitude * 1.2f,
+                    _ => maxMagnitude
+                };
+            }
+        }
+
+        private int MaximumGears
+        {
+            get
+            {
+                return Level switch
+                {
+                    5 => 12,
+                    >= 3 => 7,
+                    _ => 5
+                };
+            }
+        }
         
         // Other Variables
         private int _gearNumber;
+        private bool _isActivated;
+        private static readonly int OverclockIndex = Animator.StringToHash("Overclock");
+        private static readonly int SpinIndex = Animator.StringToHash("Spin");
 
         private void Update()
         {
-            Spin();
+            Spin(1.3f);
+            if (!Input.GetMouseButtonDown(0) || _isActivated || Level != 6) return;
+            
+            _isActivated = true;
+            activateMmFeedbacks.PlayFeedbacks();
         }
         
         public override void Detonate()
@@ -47,10 +86,15 @@ namespace _Scripts.Projectiles
             var gearObjects = GameObject.FindGameObjectsWithTag("Prop-Gear");
             foreach (var gearObject in gearObjects)
             {
-                gearObject.GetComponent<Animator>().SetTrigger(SpinIndex);
+                gearObject.GetComponent<Animator>().SetTrigger(_isActivated ? OverclockIndex : SpinIndex);
+                if (Level >= 4)
+                {
+                    DamageHandler.i.HandleDamage(gearObject.transform.position, Radius,
+                        gearSpinDamage, DamageHandler.DamageType.Circular);
+                }
             }
             
-            _gearNumber = Math.Min(gearObjects.Length, maximumGears);
+            _gearNumber = Math.Min(gearObjects.Length, MaximumGears);
             if (_gearNumber == 0)
             {
                 defaultMmFeedbacks.PlayFeedbacks();
@@ -63,6 +107,20 @@ namespace _Scripts.Projectiles
             
             yield return new WaitForSeconds(2.0f);
             DealDamage();
+
+            if (_isActivated)
+            {
+                foreach (var gearObject in gearObjects)
+                {
+                    var gearPos = gearObject.transform.position;
+                    Instantiate(GameAssets.i.explosionFX, gearPos, Quaternion.identity, gearObject.transform);
+                    EventBus.Broadcast(EventTypes.DestroyTerrain, gearPos, Radius, 1, 
+                        DestroyTypes.Circular);
+                    DamageHandler.i.HandleDamage(gearPos, Radius,
+                        gearExplosionDamage, DamageHandler.DamageType.Circular);
+                    Destroy(gearObject, 0.3f);
+                }
+            }
             
             Instantiate(gearBackgroundPrefab, pos, Quaternion.identity);
         }
