5f76d277741be4ca1c14ffad85448cb536c1ada1
rhit-yangh6 <yangh6@rose-hulman.edu>
2024-06-30 20:31:25 -0700

    delete unused assets, molotov upgrades

--- Assets/_Scripts/Projectiles/MolotovProjectile.cs
+++ Assets/_Scripts/Projectiles/MolotovProjectile.cs
@@ -6,13 +6,83 @@ namespace _Scripts.Projectiles
 {
     public class MolotovProjectile : LaunchedProjectile
     {
-        [SerializeField] private int flameNumber = 10;
         [SerializeField] private GameObject flamePrefab;
-        [SerializeField] private float flameLifeSpan = 2.5f;
-        [SerializeField] private float flameDamage = 3f;
         [SerializeField] private float flameRadius = 0.23f;
-        [SerializeField] private float flameSpreadRange = 160f;
-        [SerializeField] private float flameSpeed = 4f;
+        [SerializeField] private float flameSpeed = 3.5f;
+        
+        protected override float Damage
+        {
+            get
+            {
+                return Level switch
+                {
+                    >= 2 => damage * 1.2f,
+                    _ => damage
+                };
+            }
+        }
+        
+        protected override float MaxMagnitude
+        {
+            get
+            {
+                return Level switch
+                {
+                    >= 2 => maxMagnitude * 1.1f,
+                    _ => maxMagnitude
+                };
+            }
+        }
+
+        private int FlameNumber
+        {
+            get
+            {
+                return Level switch
+                {
+                    6 => 20,
+                    >= 3 => 13,
+                    _ => 10,
+                };
+            }
+        }
+
+        private float FlameDamage
+        {
+            get
+            {
+                return Level switch
+                {
+                    >= 4 => 3,
+                    _ => 2
+                };
+            }
+        }
+        
+        private float FlameLifeSpan
+        {
+            get
+            {
+                return Level switch
+                { 
+                    5 => 3.5f,
+                    _ => 2f
+                };
+            }
+        }
+
+        private float FlameSpreadRange
+        {
+            get
+            {
+                return Level switch
+                {
+                    6 => 160f,
+                    >= 4 => 140f,
+                    _ => 130f,
+                };
+            }
+        }
         
         private void Update()
         {
@@ -28,15 +98,15 @@ namespace _Scripts.Projectiles
         public void SpawnFlames()
         {
             Vector2 pos = transform.position;
-            for (var i = 0; i < flameNumber; i++)
+            for (var i = 0; i < FlameNumber; i++)
             {
                 var derivedObject = Instantiate(flamePrefab, pos + Vector2.up, Quaternion.identity);
                 var derivedProjectile = derivedObject.GetComponent<FlameProjectile>();
                 var derivedRb2d = derivedObject.GetComponent<Rigidbody2D>();
-                derivedProjectile.SetParameters(flameDamage, flameRadius);
-                derivedProjectile.SetLifeSpan(flameLifeSpan);
+                derivedProjectile.SetParameters(FlameDamage, flameRadius);
+                derivedProjectile.SetLifeSpan(FlameLifeSpan);
 
-                var angle = Random.Range(-flameSpreadRange / 2, flameSpreadRange / 2);
+                var angle = Random.Range(-FlameSpreadRange / 2, FlameSpreadRange / 2);
                 derivedRb2d.velocity =  Geometry.Rotate(Vector2.up * flameSpeed, angle);
             }
         }
